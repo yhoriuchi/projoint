c7f23ece83530b1f1f39553dd85c1acc75d72533
yhoriuchi <yusaku.horiuchi@gmail.com>
2022-03-16 14:14:05 -0400

    revised check_distribution

---
+++ R/check_distribution.R
@@ -0,0 +1,36 @@
+#' Check the distribution of levels for each attribute
+#'
+#' After generating test responses, it is worth checking the distribution of levels for each attribute using this function.
+#'
+#' @import dplyr
+#' @import rlang
+#' @param .data A data frame specifically for conjiont analysis (use reshape_conjiont())
+#' @return A data frame
+#' @export
+#'
+
+check_distribution <- function(.data){
+
+  attribute <- level <- frequency <- NULL
+
+  out <- NULL
+
+  for (i in seq(from = 4, to = ncol(.data) - 2, by = 1)){
+
+    att <- names(.data)[i]
+    var_quo <- rlang::sym(att)
+
+    temp <- count(.data, !!var_quo)%>%
+      mutate(attribute = att) %>%
+      set_names(c("level", "frequency", "attribute"))
+
+    out <- bind_rows(out, temp)
+
+  }
+
+  out %>%
+    select(attribute, level, frequency) %>%
+    return()
+
+}
+
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ A package to avoid measurement error bias in conjoint analysis
 
 ### Notes:
 
-- The binary profile names shoud be {"Something 1", "Something 2"} or ({Something A", "Something B"}.
+- The binary profile names should be {"Something 1", "Something 2"} or ({Something A", "Something B"}.
