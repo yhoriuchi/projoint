42ba1635c98ca8b98ad6ffb6624ee6cddf2be7af
yhoriuchi <yusaku.horiuchi@gmail.com>
2022-04-20 10:35:57 -0400

    Update read_Qualtrics.R

--- R/read_Qualtrics.R
+++ R/read_Qualtrics.R
@@ -1,15 +1,5 @@
-#' Reads and lightly processes a CSV file downloaded from Qualtrics
-#'
-#' This function reads a Qualtrics-produced CSV file and removes the first two rows, preserving headers.
-#' The output data frame is in wide format; to convert to long format, see \dQuote{reshape_conjoint()}
-#'
-#' @import readr
-#' @param .file The file name containing Qualtrics responses, preferably ending in ".csv"
-#' @return A data frame containing Qualtrics responses, suitable for re.
-#' @export
-#'
 read_Qualtrics <- function(.file){
-  
+
   if(!exists(.file)){
     stop("Error: The specified file cannot be found.")
   }
@@ -17,7 +7,7 @@ read_Qualtrics <- function(.file){
   colnames <- .file %>%
     read_csv() %>%
     names()
-  
+
   qualtrics <- read_csv(.file, skip = 2)
   colnames(qualtrics) <- colnames
 
