7f51a8e1c21f25fabcee16c2ceb311ad23f52833
yhoriuchi <yusaku.horiuchi@gmail.com>
2023-07-21 22:06:50 -0400

    Add the functions to relabel and reorder attributes and levels
    This must be super useful to applied researchers!

---
+++ R/out1_arranged.R
@@ -0,0 +1,14 @@
+#' Re-shaped example data set 1 (REORGANIZED)
+#'
+#' This ...
+#' 
+#' THIS VERSION IS AFTER MANUAL RE-LABELLING AND REORDERING.
+#'
+#' @format ## `out1_arranged`
+#'  A projoint object of class `projoint_data` ready to pass to `projoint()`.
+#' \describe{
+#'   \item{To be written}{To be written}
+
+#' }
+#' @source To be written
+"out1_arranged"
---
+++ data-raw/read_save_labels.R
@@ -0,0 +1,22 @@
+# Ref: https://github.com/mvuorre/exampleRPackage/blob/master/data-raw/preprocess.R
+
+library(tidyverse)
+library(projoint)
+
+# Data with the flipped repeated tasks
+
+outcomes <- paste0("choice", seq(from = 1, to = 8, by = 1))
+outcomes <- c(outcomes, "choice1_repeated_flipped")
+out1 <- reshape_projoint(.dataframe = exampleData1, 
+                         .idvar = "ResponseId", 
+                         .outcomes = outcomes,
+                         .outcomes_ids = c("A", "B"),
+                         .alphabet = "K", 
+                         .repeated = TRUE,
+                         .flipped = TRUE)
+
+save_labels(out1, "data-raw/labels_original.csv")
+out1_arranged <- read_labels(out1, "data-raw/labels_arranged.csv")
+
+# Apply pre-processing...
+usethis::use_data(out1_arranged, overwrite = TRUE)
