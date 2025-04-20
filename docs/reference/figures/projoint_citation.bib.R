06fb81142f349e31bad454306c4912604dbb6c8a
Yusaku Horiuchi <yusaku.horiuchi@gmail.com>
2025-04-20 08:02:54 -0400

    Track docs/ folder for pkgdown site deployment

---
+++ docs/reference/figures/projoint_citation.bib.R
@@ -0,0 +1,7 @@
+@article{clayton2025correcting,
+  title={Correcting Measurement Error Bias in Conjoint Survey Experiments},
+  author={Clayton, Katherine and Horiuchi, Yusaku and Kaufman, Aaron R and King, Gary and Komisarchik, Mayya},
+  journal={American Journal of Political Science},
+  year={Forthcoming},
+  note={Pre-print available at \url{https://gking.harvard.edu/conjointE}}
+}
